36e1c14449525e0fbb5310f0fd6f5c020a1b659f
zero-direwolf <zero.direwolf@gmail.com>
2017-01-24 03:21:39 +0530

    adding for loop

---
+++ python basics/for_loop.py
@@ -0,0 +1,12 @@
+#implimentation of for loop
+example_values = [22,5,22,1,4,6,8,4,86,9,88,59,7,44,52,8,3,48,2,185,21,58,84,35,84354,84,5,3,5,32,85,35,85,35,43,43,54]
+
+# assigning an array
+for example_numbers in example_values:
+	print(example_numbers)
+	pass
+print('adding a space for the next part of the program')
+# for using range
+for x in range(1,10):
+	print(x)
+	pass

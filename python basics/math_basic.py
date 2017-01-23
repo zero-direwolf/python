a730e43c5017617c4b539d721849ae8c8398f8a1
zero-direwolf <zero.direwolf@gmail.com>
2017-01-23 19:20:38 +0530

    adding math variables and changes in print

---
+++ python basics/math_basic.py
@@ -0,0 +1,19 @@
+# math operations
+
+add_1 = 5 + 7   #addition
+
+sub_1 = 7 - 5   # substraction
+
+mul_1 = 3 * 4   # multiplication    
+
+div_1 = 4 / 3   # division  
+
+pow_1 = 4**4    # to the power of
+
+# display the variables
+
+print(add_1)
+print(sub_1)
+print(mul_1)
+print(div_1)
+print(pow_1)

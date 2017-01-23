e46f54c2b666cebeb1ef53ed57c3034c60b68d7a
zero-direwolf <zero.direwolf@gmail.com>
2017-01-23 18:59:24 +0530

    adding print basics with hashes

--- python basics/print_basics.py
+++ python basics/print_basics.py
@@ -2,13 +2,13 @@ print('upload 1')
 
 print( "upload 2")
 
-print(' they\'re gone')
+print(' they\'re gone') # the '\' ignores the variable in front of it
 
 print("they're gone")
 
 print(5)
 
-print(str(5) + 'ton')
+print(str(5) + 'ton') # concatenate two different data types by conversion
 
 print(int('5') + 2)
 

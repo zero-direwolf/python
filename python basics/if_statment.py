862be5795f9f04c92f20f6119f695e80d5e1143c
zero-direwolf <zero.direwolf@gmail.com>
2017-01-24 03:35:28 +0530

    adding if statement

---
+++ python basics/if_statment.py
@@ -0,0 +1,28 @@
+# if condition/statement
+a = 5 
+b = 6
+c = 3
+x = 5
+y = 4 
+z = 100
+
+#checking conditions
+if b > a:
+	print('b is greater tha a')
+	pass
+if z > a + b + c + x + y:
+	print('z is the greatest')
+	pass
+if a == x:
+	print('same value')
+	pass
+if y!=a:
+	print('value not equal')
+	pass
+#using else condition
+
+if a == y:
+	print('the values are same')
+	pass
+else:
+	print('the values are not same')

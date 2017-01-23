039bff9f95f066288a2fce58b668f588eedace95
zero-direwolf <zero.direwolf@gmail.com>
2017-01-24 03:05:06 +0530

    while loop addition

---
+++ python basics/while_loop.py
@@ -0,0 +1,18 @@
+#while loop
+
+#print till condition is true
+# example for a finite loop	
+condition_1 = 1
+
+while condition_1 < 10:
+	print(condition_1)
+	condition_1 += 1
+	pass
+
+#example for an infinite loop
+true = 1
+while true:
+	print('too fast to read')
+	pass
+
+#use delay to end or cancell build

a42b4f7ed65bc0428791da0a0bb6b8a1045a6868
zero-direwolf <zero.direwolf@gmail.com>
2017-01-24 09:18:22 +0530

    user input

---
+++ python basics/user_input.py
@@ -0,0 +1,7 @@
+#user input
+
+num1 = int(input('enter number 1 ')) #takes the input string from user and converts it into a interger type
+num2 = int(input('enter number 2 '))
+sum_num =num1+num2
+
+print('the sum of the 2 numbers is ', sum_num)

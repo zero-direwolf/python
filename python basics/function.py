9493e268faaa4d72b06c500c3e1797b3aabd72e6
zero-direwolf <zero.direwolf@gmail.com>
2017-01-24 08:03:40 +0530

    function parameters

--- python basics/function.py
+++ python basics/function.py
@@ -7,3 +7,13 @@ def example_function():
 
 #to call a function
 example_function()
+
+# function parameters
+def example_addtion(num1,num2):
+	answer = num1 + num2
+	print('the first number =',num1)
+	print('the second number =',num2)
+	print('the sum of the numbers =',answer)
+	pass
+#to the function
+example_addtion(8,5)

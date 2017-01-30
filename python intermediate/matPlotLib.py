6916b79ce75707b9bedd773789901eb2c903f86d
zero-direwolf <zero.direwolf@gmail.com>
2017-01-30 19:19:56 +0530

    matplotlib plot function implimentation

---
+++ python intermediate/matPlotLib.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt
+
+x=[5,2,6,8,7,4]
+y=[6,2,5,8,8,6]
+print(len(x))
+print(len(y))
+plt.plot(x,y,'rx')
+plt.title('new chart')
+plt.xlabel('x axis')
+plt.ylabel('y axis')
+plt.show()

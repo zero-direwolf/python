d4ccbb873add28ae2db3128fb898a409ad28e3e2
zero-direwolf <zero.direwolf@gmail.com>
2017-02-23 21:13:46 +0530

    adding pickle and linearRegressionMulti.pickle

--- machine learning/linearReg/linearRegMulti.py
+++ machine learning/linearReg/linearRegMulti.py
@@ -3,6 +3,7 @@ import numpy as np   #we can perform matrix operations using this library
 from sklearn.linear_model import LinearRegression  #the main library to perform Linear Regression
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
+import pickle #This is to store the trained classifier
 
 #loading the data into a variable data
 data = pd.read_csv('ex1data2.txt')
@@ -14,7 +15,16 @@ X_train , X_test , y_train , y_test = train_test_split(X,y,test_size=0.3)
 
 clf = LinearRegression()
 clf.fit(X_train,y_train)  #training the model with the fit method
- 
+
+#This saves the trained classifier into the file named 
+#linearRegressionMulti which can be called again later 
+#instead of retraining the classifier 
+with open('linearRegressionMulti.pickle','wb') as f: 
+	pickle.dump(clf,f)
+
+pickle_in = open('linearRegressionMulti.pickle','rb')
+clf = pickle.load(pickle_in)
+
 pred = clf.predict(X_test)  # we can now predict the random values we kept aside
 
 m = clf.coef_
